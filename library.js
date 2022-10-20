cea6d3a53252bdfcbfbb2c3f1a8f79af94680a8c
v_slechta <slechtav@outlook.com>
2022-10-20 12:23:42 +0100

    Update library.js

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+/*shopForBeans() expects no arguments and returns a promise which will resolve to a string of a bean type.*/
+
 const shopForBeans = () => {
   return new Promise((resolve, reject) => {
 	const beanTypes = ['kidney', 'fava', 'pinto', 'black', 'garbanzo'];
@@ -10,6 +12,9 @@ const shopForBeans = () => {
 })
 }
 
+/*soakTheBeans() expects a bean type string as an argument and returns a promise which resolves
+to a boolean indicating whether or not the beans are softened.*/
+
 let soakTheBeans = (beanType) => {
    return new Promise((resolve, reject) => {
      console.log('Time to soak the beans.');
@@ -19,6 +24,9 @@ let soakTheBeans = (beanType) => {
       }, 1000);
   });
 }
+
+/*cookTheBeans() expects a boolean as an argument and, if that value is true, returns a promise 
+which will resolve to a string announcing that dinner is ready.*/
 
 let cookTheBeans = (isSoftened) => {
   return new Promise((resolve, reject) => {
